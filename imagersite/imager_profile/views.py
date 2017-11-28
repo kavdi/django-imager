46e3d7ba896d374bb7383f4b65ffc55fbe0fbbbc
kavdihodgson <kavdyjh@gmail.com>
2017-11-27 17:02:46 -0800

    model made.

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,3 +1,15 @@
+"""Views for site."""
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+
+def home_view(request):
+    """Home view callable, for the home page."""
+    import pdb; pdb.set_trace()
+    template = loader.get_template('imagersite/home.html')
+    response_body = template.render({'name': 'Playa'})
+    return HttpResponse(response_body)
