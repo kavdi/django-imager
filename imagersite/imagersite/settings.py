71997f53515122ce053a5b04098048584980e588
kavdihodgson <kavdyjh@gmail.com>
2018-01-04 14:52:27 -0800

    still on s3.

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -25,10 +25,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'ec2-34-210-23-192.us-west-2.compute.amazonaws.com',
-    '34.210.23.192',
-]
+# ALLOWED_HOSTS = [
+#     'ec2-34-210-23-192.us-west-2.compute.amazonaws.com',
+#     '34.210.23.192',
+# ]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
